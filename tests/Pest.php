4241547b9c78c174de84827f07ac50f84b3082db
Yezz Design <info@yezzmedia.com>
2026-05-13 11:29:51 +0200

    test: scope Pest suites from package paths

--- tests/Pest.php
+++ tests/Pest.php
@@ -4,4 +4,4 @@ declare(strict_types=1);
 
 use YezzMedia\OpsBackups\Tests\OpsBackupsTestCase as TestCase;
 
-uses(TestCase::class)->in('Feature', 'Unit');
+uses(TestCase::class)->in(__DIR__.'/Feature', __DIR__.'/Unit');
